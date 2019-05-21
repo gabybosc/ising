a40bcbc52a0308e65d3eb334f2b3c566be8a735a
gabybosc <gabybosc@gmail.com>
2019-05-21 16:15:33 -0300

    ahora sí que anda

--- importance_sampling.py
+++ importance_sampling.py
@@ -64,13 +64,41 @@ plt.title('Correlacion')
 plt.legend()
 plt.grid()
 
+# x_N = datos[idx,1:]
+# # para cada delta, separar la tira de x en tiras mas cortas y hacer un promedio sobre ellas
+# ck = np.zeros(len(ks))
+# for j in np.arange(inicio,fin,paso): # loop para subtiras de x_N
+#     x = x_N[j:j+paso]
+#     for i,k in enumerate(ks): # inicio loop k
+#         corr = func_corr(x, k)
+#         ck[i] = ck[i] + corr
+# ck_mean = ck/((fin-inicio)/paso)
+# plt.plot(ks, ck_mean, label='delta = {}'.format(delta[idx]))
+
 
 # Calculo de la integral (Creo que debe dar sqrt(2pi) )
-term = 100
-x = datos[idx, term:]
-x.sort()
-y = x**2 * np.exp(-x**2/2)
-print('Si hacemos la integral, nos da I - 2pi = {}'.format(trapz(y,x)- np.sqrt(2*np.pi)))
-print('Si sumamos, nos da I - 2pi = {}'.format(np.sum(x**2)/len(x) - 1))
+datos_integral = np.loadtxt('../../computacional/datos/importance_sampling_integral.txt')
+x_diezmado = np.array([datos_integral[4*i] for i in range(int(len(datos_integral)/4))])
+
+tamanios = range(1000, len(x_diezmado), 500)
+termal = range(0, 900, 100)
+matriz = np.zeros((len(tamanios), len(termal)))
+plt.figure()
+for i,N in enumerate(tamanios):
+    for j,term in enumerate(termal):
+        x = x_diezmado[term:N]
+        # x.sort()
+        # y = x**2 * np.exp(-x**2/2)
+        # I = trapz(y,x)- np.sqrt(2*np.pi)
+        I = np.sum(x**2)/len(x) - 1
+        matriz[i,j] = I
+    plt.plot(termal,matriz[i,:],label=tamanios[i])
+
+plt.legend()
+
+"""si hacemos esto pero sin diezmar los x, nos da mal, creemos que es instructivo poner el grafiquito malo para mostrar que efectivamente cambia si no consideramos la correlacion."""
+
+    # print('Si hacemos la integral, nos da I - 2pi = {}'.format(trapz(y,x)- np.sqrt(2*np.pi)))
+    # print('Si sumamos, nos da I - 2pi = {}'.format(np.sum(x**2)/len(x) - 1))
 # que se usa como dx??? opcion: ordenar el vector x y usar diferencias entre pasos consecutivos
 #dx = delta[idx]
---
+++ integral.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> /* memset */
+#include <math.h>
+#include <time.h>
+#include <unistd.h> /* close */
+
+
+double aleatorio();
+
+//necesito un programa que me de p* o p* exp(), con una dada probabilidad
+//La funcion es f(x) = exp{-x²/2} / sqrt(2pi)
+
+
+//correr esto para distintos deltas, para hallar el delta critico, donde la correlacion es minima.
+int main(){
+  //elegimos primero un x_0 random
+  //elegimos un x_1 random que esté a una distancia delta de x0
+  FILE *fp;
+  int N, i, m;
+  double xi, xf, delta_x, random, p, delta; //xi es inicial y xf es final
+  double *serie;
+  N = 20000;
+  delta = 3;
+  srand(time(NULL));
+  // delta = 5;
+  serie = (double*)malloc(N*sizeof(double));
+
+  xi = 2 *aleatorio()-1;
+  *serie = xi;
+
+
+	char fn[100];
+	sprintf(fn,"/home/gabybosc/computacional/datos/importance_sampling_integral.txt");
+	fp = fopen(fn, "w");
+
+
+  for(m = 1; m < N; m++){
+    xf = delta * (2 * aleatorio() -1) + xi; //esto elije el xf de modo tal que sea aleatorio y que quede entre xi-delta y xi+delta
+
+    delta_x = (pow(xf,2) - pow(xi,2)) / 2;
+    random = aleatorio();
+
+    //la proba de aceptar x1 viene dada por exp{x0²/2-x1²/2}. Comparamos esta proba con un numero random para ver si lo aceptamos.
+    p = exp(-delta_x);
+    if (p > random){
+      *(serie+m) = xf;
+      xi = xf;
+    }
+    else{
+      *(serie+m) = xi;
+    }
+
+
+  } //cierra el loop en la serie
+  for (i=0; i<N; i++){
+		fprintf(fp, "\t%f", *(serie+i));
+	}
+
+  free(serie);
+  return 0;
+}
+
+double aleatorio(){
+  double random;
+
+  random = (float)rand()/(float)RAND_MAX;
+
+  return random;
+}
