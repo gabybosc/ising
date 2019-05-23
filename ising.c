4d3683861603a85d5e4f99fc6caffceaeb69fe2b
gabybosc <gabybosc@gmail.com>
2019-05-23 16:21:22 -0300

    flipea espines con B = 1 (si es mas grande cambia todo)

--- ising.c
+++ ising.c
@@ -3,17 +3,36 @@
 #include <math.h>
 #include <time.h>
 
-#define MAXFILENAME 100
+#define N 32
+#define B 1
+#define PASO 1
 
-int poblar(int *red, int N, double P);
-int imprimir(int *red, int N);
+int poblar(int *red);
+int flipear(int *red, int *s);
+int imprimir(int *red);
 
 //------------MAIN-------------
 int main(){
-	FILE *fp;
+	// FILE *fp;
+	int *red, i, *s, j;
+	float ham, mag; //Hamiltoniano, magnetizacion
+	red = (int*)malloc(N*N*sizeof(int));
+	s = (int*)malloc((N*N+1)*sizeof(int));
+	srand(time(NULL));
 
-	sprintf(fn,"/home/gabybosc/computacional/datos/nombredelarchivo.txt");
-	fp = fopen(fn, "w"); //"a" es append, mientras que "w" sobreescribe
+	poblar(red);
+	imprimir(red);
+	for(j=0; j<PASO; j++){
+		printf("%nueva red\n");
+		flipear(red, s);
+		imprimir(red);
+	}
+	// ham = -B * (float)s/PASO;
+	// mag = (float)s/PASO;
+	// free(red);
+	// printf("%f %f\n",ham, mag);
+	// sprintf(fn,"/home/gabybosc/computacional/datos/nombredelarchivo.txt");
+	// fp = fopen(fn, "w"); //"a" es append, mientras que "w" sobreescribe
 
 return 0;
 }
@@ -21,7 +40,7 @@ return 0;
 
 //----------FUNCIONES----------
 
-int poblar(int *red, int N){
+int poblar(int *red){
 //llena la red cuadrada de lado N con prob P
 	int i;
 	float P, random;
@@ -37,7 +56,40 @@ return 0;
 }
 
 
-int imprimir(int *red, int N){
+int flipear(int *red, int *s){
+//flipea un s_inicial a un s_final. Si la energía baja, lo acepta. Si aumenta, lo acepta con una proba P.
+	int si,i; //si = s en el lugar i
+	float P, random;
+	double delta_E;
+
+	for (i = 0; i < N*N; i++){
+		*s += *(red+i);
+	}
+
+	for (i = 0; i < N*N; i++){
+		random = (float)rand()/(float)RAND_MAX;
+		si = *(red+i);
+		delta_E = 2 * B *si;
+		P = exp(-delta_E); //proba de aceptar
+
+		if(delta_E < 0){
+			*(red+i) = -si;
+			*(s+i+1) = *(s+i) - 2*si;
+			// printf("%d\n", si);
+		}
+		else if(random < P){
+			*(red+i) = -si;
+			*(s+i+1) = *(s+i) - 2*si;
+		}
+		else{
+			*(s+i+1) = *(s+i);
+		}
+	}
+
+return 0;
+}
+
+int imprimir(int *red){
 //para mostrar la red creada
 	int i,j;
 	for (i=0; i<N; i++)
