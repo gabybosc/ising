b17a5c5acf06713d6edafc8ee58dc63cc0cb5f44
gabybosc <gabybosc@gmail.com>
2019-06-14 15:41:11 -0300

    correlacion

--- ising_corr_crit.c
+++ ising_corr_crit.c
@@ -5,7 +5,7 @@
 
 #define N 32
 #define B 0.0
-//#define J 0.01
+// #define J 0.8
 #define SIZE 400000000
 
 int poblar(int *red);
@@ -19,10 +19,10 @@ float func_corr(float *x, int n, int k);
 //------------MAIN-------------
 int main(){ //(int argc,char *argv[])
 	// Para cada temperatura calculamos los observables para SIZE pasos correlacionados
-	// Guarda un txt que devuelve la correlacion ya promediada 
+	// Guarda un txt que devuelve la correlacion ya promediada
 	srand(time(NULL));
 	clock_t tic = clock();
-	
+
 	FILE *fp;
 	int *red, *magnetizacion, i,l,k;
 	//float *energia;
@@ -30,43 +30,43 @@ int main(){ //(int argc,char *argv[])
 	red = (int*)malloc(N*N*sizeof(int));
 	magnetizacion = (int*)calloc(SIZE, sizeof(int));
 	//energia = malloc(SIZE* sizeof(float));
-	
+
 	int k_max = 300; //NOTA: Esto no es k, es k per site: (k/(N*N))
 	correlacion = malloc(k_max* sizeof(float));
-	
+
 	//correlacion sobre tiras de longitud n_tira (luego de pasos de termalizacion)
 	int n_tira = 4000000;
 	int termalizacion = 20000;
 	int cant_tiras = (SIZE-termalizacion)/n_tira;
 	float *x;
 	x = malloc(n_tira * sizeof(float));
-	
-	float J;
-	
+
+	float J = 0.95;
+
 	char fn[50];
 	sprintf(fn,"ising_correlacion_Jcrit.txt");
 	fp = fopen(fn, "w");
 	fprintf(fp, "B %f; J [0.4; 0.65]; cadena %d, term %d, %d tiras de %d pasos, kps_max %d\n",B,SIZE,termalizacion,cant_tiras,n_tira,k_max);
 	fprintf(fp, "Para cada J, C(k) para magnetizacion\n");
 
-	for (J=0.4; J<0.65; J+=0.05){//loop J
+	// for (J=0.2; J<0.85; J+=0.2){//loop J
 		poblar(red);
-		
+
 		for (l = 0; l < N*N; l++){
 			*magnetizacion=0;
 		} //set magnetizacion en 0
-		
+
 		for (l = 0; l < N*N; l++){
 			*magnetizacion += *(red+l);
 		} //calculo magnetizacion total del primer estado
-		
+
 		//energia_interaccion(red, energia, J); //energia de interaccion del primer estado
 		//*energia = *energia - B * *(magnetizacion); // le sumo la energia del campo B
 
 		// OJO: en correlacion, funcion flipear cambia para guardar todos los pasos
 		flipear(red, magnetizacion, J);
-		
-		
+
+
 		for (k=0; k<k_max; k++){
 			*(correlacion+k) = 0;
 		}//set correlacion a 0
@@ -85,8 +85,8 @@ int main(){ //(int argc,char *argv[])
 			fprintf(fp, "%f ",*(correlacion+k));
 		}//k loop para imprimir correlacion
 		fprintf(fp, "\n");
-	}//end loop J
-	
+	// }//end loop J
+
 	free(red);
 	free(magnetizacion);
 	//free(energia);
@@ -154,7 +154,7 @@ return sum_sj;
 }
 
 
-int flipear(int *red, int *magnetizacion, float J){ //hacerlo de forma secuencial nos puede traer problemas para T bajas
+int flipear(int *red, int *magnetizacion, float beta){ //hacerlo de forma secuencial nos puede traer problemas para T bajas
 //flipea un s_inicial a un s_final. Si la energía baja, lo acepta. Si aumenta, lo acepta con una proba P.
 	int si,i,j,sum_sj; //si = s en el lugar i
 	float P, random;
@@ -168,7 +168,7 @@ int flipear(int *red, int *magnetizacion, float J){ //hacerlo de forma secuencia
 		si = *(red+i);
 		sum_sj = sumar_sj(red, i);
 
-		delta_E = 2 * si * (J * sum_sj + B);
+		delta_E = 2 * si * beta* (J * sum_sj + B);
 		P = exp(-delta_E); //proba de aceptar
 
 		if(delta_E < 0){
@@ -221,7 +221,7 @@ float func_corr(float *x, int n, int k){
 	float media_x = 0;
 	float media_sqx = 0;
 	float numerador,denominador;
-	
+
 	for (i=0; i<(n-k); i++){
 		media += *(x+i+k) * *(x+i); //valor medio de x(i)*x(i+k)
 		media_x += *(x+i); 		// valor medio de x
@@ -231,7 +231,7 @@ float func_corr(float *x, int n, int k){
 	media_x = media_x/(n-k);
 	media_x = pow(media_x,2);	// OJO! Ahora media_x es (mean(x))**2
 	media_sqx = media_sqx/(n-k);
-	
+
 	numerador = media - media_x;
 	denominador = media_sqx - media_x;
 
--- ising_meanmag.c
+++ ising_meanmag.c
@@ -3,15 +3,15 @@
 #include <math.h>
 #include <time.h>
 
-#define B 1 	// campo magnetico
+#define B 1.0 	// campo magnetico
 #define N 32 	// lado de la red
 #define J 0 	// interaccion entre particulas
 #define PASO 10 	// descorrelacion: PASO*SIZE*N^2
-#define SIZE 1000000 	// tamaño de vectores diezmados
+#define SIZE 100000 	// tamaño de vectores diezmados
 #define TERM 20000	// pasos de termalizacion
 
 int poblar(int *red);
-int flipear(int *red, int *magnetizacion, float B);
+int flipear(int *red, int *magnetizacion, float beta);
 int imprimir(int *red);
 
 //------------MAIN-------------
@@ -78,7 +78,7 @@ return 0;
 }
 
 
-int flipear(int *red, int *magnetizacion, float B){
+int flipear(int *red, int *magnetizacion, float beta){
 //flipea un s_inicial a un s_final. Si la energía baja, lo acepta. Si aumenta, lo acepta con una proba P.
 	int si,i,j,l; //si = s en el lugar i
 	float P, random;
@@ -90,7 +90,7 @@ int flipear(int *red, int *magnetizacion, float B){
 			random = (float)rand()/(float)RAND_MAX;
 			i = ((float)rand()/(float)RAND_MAX)* N*N;
 			si = *(red+i);
-			delta_E = 2 * B *si;
+			delta_E = 2 * beta * B *si;
 			P = exp(-delta_E); //proba de aceptar
 
 			if(delta_E < 0){
